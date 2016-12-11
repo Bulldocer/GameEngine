62d023de7990f572d7e7daffcb3409bc8109c181
jar19 <jar19@DESKTOP-D4OM5D8>
2016-12-11 19:44:57 +0100

    Commit bueno de antes

--- GameEngine/GameEngine/Animation.h
+++ GameEngine/GameEngine/Animation.h
@@ -7,7 +7,7 @@ public:
 	Animation();
 	~Animation();
 	const int ANIMATION_FRAMES = 0;
-	SDL_Rect gSpriteClips[0];
+	SDL_Rect gSpriteClips[ANIMATION_FRAMES];
 	LTexture gSpriteSheetTexture;
 	int frame = 0;
 	void render();
